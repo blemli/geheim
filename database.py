2095001bcd0bdc1565800b95a81e358c8bcde0fd
grafst <grafst@users.noreply.github.com>
2023-02-22 09:21:34 +0100

    fix missing connection in get tables

--- database.py
+++ database.py
@@ -1,5 +1,4 @@
 import os
-
 import psycopg2
 
 
@@ -9,7 +8,7 @@ def _connect(connection_string):
         connection = psycopg2.connect(connection_string)
         print("Connection to PostgreSQL DB successful")
     except psycopg2.OperationalError as e:
-        print(f"The error '{e}' occurred")
+        print(f"could not connect to databse: '{e}'")
     return connection
 
 
@@ -30,21 +29,21 @@ def _execute_query(connection, query):
 
 
 def init(db):
-	create_secrets_query = """
+    create_secrets_query = """
 CREATE TABLE IF NOT EXISTS secrets (
   id UUID PRIMARY KEY NOT NULL,
   ciphertext TEXT NOT NULL,
   saved DATE NOT NULL DEFAULT CURRENT_DATE
 );
 """
-	_execute_query(db, create_secrets_query)
+    _execute_query(db, create_secrets_query)
 
 
 def get_tables(db):
     query = """SELECT table_schema,table_name
 FROM information_schema.tables
 ORDER BY table_schema,table_name;"""
-    return _execute_query(query)
+    return _execute_query(db, query)
 
 
 def test():
